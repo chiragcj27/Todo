c2e14e34d4a63013cab3e9a52b8c0ab78a694cb6
Chirag Jain <cj27chirag@gmail.com>
2024-01-02 19:25:33 +0530

    Express & types

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+
+app.post('/todo', function(res, req) {
+
+})
+
+app.get('/todos', function(res,req) {
+
+})
+
+app.put("/completed", function(res,req) {
+
+})
