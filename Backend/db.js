e9af8b7c8da2824314330d69d7e0419166143a0c
Chirag Jain <cj27chirag@gmail.com>
2024-01-03 23:40:43 +0530

    db files

---
+++ Backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { boolean } = require("zod");
+
+mongoose.connect('mongodb+srv://chiragcj27work:qOdviooDoNT0El1q@cluster0.od4gxzg.mongodb.net/todo');
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    comleted: boolean
+})
+
+const todo = mongoose.model('todos', todoSchema);
+
+module.exports = {
+    todo: todo
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-
+const { createTodo, updateTodo } = require("./types")
 
 app.use(express.json());
 
 app.post('/todo', function(res, req) {
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.statusCode(411).json({
+            msg: "You sent the wrong inputs"
+        })
+    }
 
 })
 
@@ -13,5 +20,11 @@ app.get('/todos', function(res,req) {
 })
 
 app.put("/completed", function(res,req) {
-
+    const updatePayload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayload);
+    if(!parsePayload.success) {
+        res.statusCode(411).json({
+            msg: "You sent the wrong inputs"
+        })
+    }
 })
